acd20c517b6b9e7f43b345b17580c7464e4847a4
Guard84 <guardsofword@gmail.com>
2024-02-08 16:29:01 +0200

    gc2

---
+++ src/components/FriendList.jsx
@@ -0,0 +1,18 @@
+import { FriendListItem } from "./FriendListItem"
+
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <li key={friend.id}>
+          <FriendListItem 
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+}
